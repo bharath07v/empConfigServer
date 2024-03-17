99bebca35d8f14d8009fe46fb25153725fdca8b5
bharath07v <slnbd7@gmail.com>
2024-03-17 18:41:06 +0530

    initial commit

---
+++ src/test/java/com/example/empConfigServer/EmpConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
